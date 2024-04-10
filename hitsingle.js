984dda64de77dc7b729ef8ef9da9498d6a9d2deb
rohanprajapati <rohanprajapati369@gmail.com>
2024-04-10 10:00:04 +0545

    Update array element in hitsingle.js

--- hitsingle.js
+++ hitsingle.js
@@ -1,7 +1,7 @@
 const axios = require("axios");
 const fs = require("fs");
 const arr = [
-  "660dae8da723d779ba277177"
+  "661423fc0365d96c7ae5b8d3"
 ]
 
 const awaitTimer = (ms) => new Promise((res) => setTimeout(res, ms));
